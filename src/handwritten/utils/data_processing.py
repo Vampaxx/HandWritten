fe02b3ec441f5156979971343de7ec53b8c22c42
Vampaxx <arjunappu1001@gmail.com>
2023-11-04 17:47:13 +0530

    data processing modified

--- src/handwritten/utils/data_processing.py
+++ src/handwritten/utils/data_processing.py
@@ -1,4 +1,16 @@
+import cv2
+import numpy as np
+import tensorflow as tf
+
+
 def preprocessing(data):
     data = data/255.0
     return data
 
+def load_image(path: str):
+    image = cv2.imread(path)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)  # Convert to grayscale
+    image = cv2.resize(image, (28, 28))
+    image = image / 255.0
+    image = image.reshape(28, 28, 1)  # Reshape to (28, 28, 1)
+    return image
