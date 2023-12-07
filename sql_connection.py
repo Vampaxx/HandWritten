6a986b2c8d62ac9e8d1e984ee86ab44694ddf685
Vampaxx <arjunappu1001@gmail.com>
2023-12-08 00:07:51 +0530

    modified sql_connector(creds)

--- sql_connection.py
+++ sql_connection.py
@@ -1,9 +1,10 @@
 import pymysql  
+import creds
 
 def sql_connector():
     connection = pymysql.connect(host='127.0.0.1',
                                 user='root',
-                                password='arjun9400508669',
+                                password=creds.sql_password,
                                 database='data1',)
     mycursor = connection.cursor()
     return connection,mycursor
