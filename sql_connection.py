6c4ee30f623482f1c2098835a0ff589a9976d9b2
Vampaxx <arjunappu1001@gmail.com>
2023-12-09 11:39:44 +0530

    requirements_&_sql_connection

--- sql_connection.py
+++ sql_connection.py
@@ -2,7 +2,7 @@ import pymysql
 import creds
 
 def sql_connector():
-    connection = pymysql.connect(host='127.0.0.1',
+    connection = pymysql.connect(host=creds.sql_host,
                                 user='root',
                                 password=creds.sql_password,
                                 database='data1',)
