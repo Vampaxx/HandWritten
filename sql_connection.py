ed0326668955dff1ec863b50f08b6e8341476aeb
Vampaxx <arjunappu1001@gmail.com>
2023-12-10 23:58:10 +0530

    sql connector with pymysql

--- sql_connection.py
+++ sql_connection.py
@@ -1,5 +1,7 @@
-import pymysql  
+import os
 import creds
+import pymysql  
+
 
 def sql_connector():
     connection = pymysql.connect(host=creds.sql_host,
@@ -9,6 +11,25 @@ def sql_connector():
     mycursor = connection.cursor()
     return connection,mycursor
 
+
+'''def sql_connector():
+    db_url = 'mysql+pymysql://root:arjun9400508669@flask_db:3306/data1'
+    print(db_url)
+
+    if db_url:
+        connection = pymysql.connect(db_url)
+    else:
+        # If DB_URL is not set, use default values
+        connection = pymysql.connect(
+            host='localhost',
+            user='root',
+            password='arjun9400508669',
+            database='data1',
+        )
+
+    mycursor = connection.cursor()
+    return connection, mycursor
+'''
 
 
 
